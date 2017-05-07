f8b1160f67c6ac2c976c802fe29d368037031afe
Jason Stewart <support@eggplantsd.com>
2017-05-07 01:26:29 -0500

    * Reverted cmd_edit default editor checking. * Check for $EDITOR first in cmd_edit._editor() to ensure that the $VISUAL value does not short-circuit `make test`.

--- utt/cmd_edit.py
+++ utt/cmd_edit.py
@@ -4,40 +4,18 @@ import subprocess
 NAME = 'edit'
 DESCRIPTION = 'Edit task log using your system\'s default editor'
 
-
 def add_args(parser):
     pass
 
-
 def execute(args):
+    _run_editor(_editor(), args.data_filename)
 
-    _run_editor(path_default_editor(), args.data_filename)
-
-
-def checkEnvVar(envVar):
-
-    szPath = os.environ.get(envVar, '')
-
-    if(len(szPath) > 0):
-        return szPath
-
-    return False
-
-
-def path_default_editor():
-
-    szEditorDefault = '/usr/bin/editor'
-
-    if(not os.path.exists(szEditorDefault)):
-        szEditorDefault = 'vi'
-
-    # NOTE: CHECK $EDITOR FIRST SINCE IT IS SET TO 'cat' ON 'make test'
-    return (
-        checkEnvVar("EDITOR") or
-        checkEnvVar("VISUAL") or
-        szEditorDefault
-    )
-
+def _editor():
+    # CHECK $EDITOR FIRST, SINCE IT IS SET TO 'cat' FOR 'make test'.
+    # AN INTERACTIVE EDITOR WILL INTERRUPT THE TEST IF $VISUAL IS SET
+    # AND CHECKED FIRST.
+    return (os.environ.get("EDITOR") or
+            os.environ.get("VISUAL", "vi"))
 
 def _run_editor(editor, data_filename):
     return subprocess.call("%s \"%s\"" % (editor, data_filename), shell=True)
