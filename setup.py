31892199d6ffe59f6cdb51c92d2bafd63c2d9cd1
Mathieu Larose <mathieu@mathieularose.com>
2017-05-21 10:59:41 -0400

    Remove argparse

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     data_files=[(os.path.join('share', 'bash-completion', 'completions'),  (os.path.join('bash', 'utt'),))],
     url="https://github.com/larose/utt",
     version=get_version(),
-    install_requires=['argparse', 'argcomplete'],
+    install_requires=['argcomplete'],
     entry_points={
         'console_scripts': [
             'utt = utt.__main__:main',
