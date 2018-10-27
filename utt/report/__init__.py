2da7b8ed4ce8402e10e5f340657fc436c10adef0
Mathieu Larose <mathieu@mathieularose.com>
2018-10-27 18:30:03 -0400

    Format code

--- utt/report/__init__.py
+++ utt/report/__init__.py
@@ -6,11 +6,13 @@ from .projects_section import ProjectsModel
 from .activities_section import ActivitiesModel
 from .details_section import DetailsModel
 
+
 class Report:
     def __init__(self, activities, start_date, end_date):
         self.summary_model = SummaryModel(activities, start_date, end_date)
         self.projects_model = ProjectsModel(activities, start_date, end_date)
-        self.activities_model = ActivitiesModel(activities, start_date, end_date)
+        self.activities_model = ActivitiesModel(activities, start_date,
+                                                end_date)
         self.details_model = DetailsModel(activities, start_date, end_date)
         self.start_date = start_date
         self.end_date = end_date
