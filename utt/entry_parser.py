8e3de186394a5418320067bfb5a40f90354bfebf
Mathieu Larose <mathieu@mathieularose.com>
2018-11-04 15:43:34 -0500

    Timezone offset must not be separated by whitespaces
    Otherwise parsing can be ambiguous.

--- utt/entry_parser.py
+++ utt/entry_parser.py
@@ -5,7 +5,7 @@ from dateutil.parser import parse
 from .entry import Entry
 
 WITH_TZ = re.compile(
-    r"(?P<date>\d{4}-\d{1,2}-\d{1,2}\s+\d{1,2}:\d{1,2})\s*(?P<timezone>[+-]{1}\d{2}:{0,1}\d{2})"
+    r"(?P<date>\d{4}-\d{1,2}-\d{1,2}\s+\d{1,2}:\d{1,2})(?P<timezone>[+-]{1}\d{2}:{0,1}\d{2})"
     r"\s+(?P<name>[^\s].*)")
 
 WITHOUT_TZ = re.compile(
