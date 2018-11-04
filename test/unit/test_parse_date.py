5ed8345211ce71bd9e9bc6fc296001ba71bb3176
Kit Choi <kit@kychoi.org>
2018-11-04 15:44:42 +0000

    _parse_date should accept a date, not a datetime, as reference

--- test/unit/test_parse_date.py
+++ test/unit/test_parse_date.py
@@ -6,22 +6,25 @@ import ddt
 from utt.report import _parse_date
 
 VALID_ENTRIES = [
-    ("monday", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(2015, 2,
-                                                                   9)),
-    ("tuesday", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(
-        2015, 2, 10)),
-    ("wednesday", datetime.datetime(2015, 2, 11, 7, 0),
-     datetime.date(2015, 2, 11)),
-    ("thursday", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(
-        2015, 2, 5)),
-    ("friday", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(2015, 2,
-                                                                   6)),
-    ("saturday", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(
-        2015, 2, 7)),
-    ("sunday", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(2015, 2,
-                                                                   8)),
-    ("2015-2-8", datetime.datetime(2015, 2, 11, 7, 0), datetime.date(
-        2015, 2, 8)),
+    ("monday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 9), True),
+    ("tuesday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 10), True),
+    ("wednesday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 11),
+     True),
+    ("thursday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 5), True),
+    ("friday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 6), True),
+    ("saturday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 7), True),
+    ("sunday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 8), True),
+    ("2015-2-8", datetime.date(2015, 2, 11), datetime.date(2015, 2, 8), True),
+    ("monday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 16), False),
+    ("tuesday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 17), False),
+    ("wednesday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 11),
+     False),
+    ("thursday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 12),
+     False),
+    ("friday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 13), False),
+    ("saturday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 14),
+     False),
+    ("sunday", datetime.date(2015, 2, 11), datetime.date(2015, 2, 15), False),
 ]
 
 
@@ -29,6 +32,6 @@ VALID_ENTRIES = [
 class ParseDate(unittest.TestCase):
     @ddt.data(*VALID_ENTRIES)
     @ddt.unpack
-    def test(self, report_date, now, expected_report_date):
-        actual_report_date = _parse_date(now, report_date)
+    def test(self, report_date, today, expected_report_date, is_past):
+        actual_report_date = _parse_date(today, report_date, is_past)
         self.assertEqual(actual_report_date, expected_report_date)
