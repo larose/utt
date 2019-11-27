2a2bd6d0150099f997c84c2b53ea1055a2bee0c5
Henrik Holm <ukrutt@gmail.com>
2019-11-27 17:58:10 -0500

    Tests to reflect discussion

--- test/unit/test_report.py
+++ test/unit/test_report.py
@@ -122,7 +122,6 @@ def test_single_day(args, activities, local_timezone):
 @pytest.mark.parametrize(
     "delta,billable",
     [
-        (dict(minutes=66), ' 1.1'),
         (dict(minutes=0), ' 0.0'),
         (dict(minutes=1), ' 0.0'),
         (dict(minutes=2), ' 0.0'),
@@ -133,9 +132,15 @@ def test_single_day(args, activities, local_timezone):
         (dict(minutes=7), ' 0.1'),
         (dict(minutes=8), ' 0.1'),
         (dict(minutes=9), ' 0.2'),
-        (dict(minutes=11), ' 0.2'),
+        (dict(minutes=14), ' 0.2'),
         (dict(minutes=15), ' 0.3'),
         (dict(minutes=30), ' 0.5'),
+        (dict(minutes=56), ' 0.9'),
+        (dict(minutes=57), ' 1.0'),
+        (dict(minutes=60), ' 1.0'),
+        (dict(minutes=62), ' 1.0'),
+        (dict(minutes=63), ' 1.1'),
+        (dict(minutes=66), ' 1.1'),
         # NOTE, utt doesn't really deal with seconds, but this is how the
         #   rounding would work if it did.
         (dict(seconds=1), ' 0.0'),
