fef59674fefdc1a51de1082a788ecef6f5df36b5
Satish Viswanathan <satish.viswanathan.in@gmail.com>
2022-12-12 17:46:52 +0530

    fix(loop): Menu Rendereing
    Had to move gum style loop inside and end every case item with clear so that the menu could be rendered properly.

--- gumlog.sh
+++ gumlog.sh
@@ -5,30 +5,34 @@ then
     exit 1
 fi
 clear
-gum style --foreground 80 --border double --border-foreground 80 --padding "1 2" --margin 1 --align center "Utt Log Form" "Use arrows or j-k to move up down"
 
 while :
 do
+gum style --foreground 80 --border double --border-foreground 80 --padding "1 2" --margin 1 --align center "Utt Log Form" "Use arrows or j-k to move up down"
     CHOICE=$(gum choose "Add Log Entry" "Edit File" "See Report" "See Log File" "Quit")
     case $CHOICE in 
         "Add Log Entry")
             PROJECT=$(gum input --prompt "Project: " --placeholder "Which Project you were wroking on? ")
+            DESC=$(gum input --prompt "${PROJECT}: Description: " --placeholder "Explain what you did in about 10-15 words...")
             test -n "$PROJECT" && SCOPE="$PROJECT: "
-            DESC=$(gum input --prompt "\"$SCOPE\"Description: " --placeholder "Explain what you did in about 10-15 words...")
             test -n "$DESC" && SCOPE="$SCOPE$DESC"
-            test -n "$SCOPE" && sh -c "utt add \"$SCOPE\"" && gum spin -s minidot --title "Log written ..." -- sleep 1 || gum spin -s monkey --title "No input provided ..." -- sleep 2
+            test -n "$SCOPE" && sh -c "utt add \"$SCOPE\"" && gum spin -s minidot --title "Log written ..." -- sleep 1 && clear || gum spin -s monkey --title "No input provided ..." -- sleep 2 && clear
             ;;
 
         "Edit File")
             sh -c "utt edit"
+            clear
             ;;
 
         "See Report")
             sh -c "utt report"
+            gum confirm "Continue?" 
+            clear
             ;;
 
         "See Log File")
-            gum pager < $UTT_LOG_FILE
+            gum pager < "$UTT_LOG_FILE"
+            clear
             ;;
         *)
             echo -n "Quitting ..." ; sleep 1
