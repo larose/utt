90165cbca5a3630d2d16a0f7be5c5e4fd876ed31
Mathieu Larose <mathieu@mathieularose.com>
2025-11-01 10:51:02 -0400

    Remove unused code

--- utt/data_structures/name.py
+++ utt/data_structures/name.py
@@ -24,6 +24,3 @@ class Name:
 
     def __str__(self):
         return self.name
-
-    def __repr__(self):
-        return "Name(" + ", ".join([self.name, self.task, self.project]) + ")"
--- utt/report/common.py
+++ utt/report/common.py
@@ -16,35 +16,6 @@ def print_dicts(dcts: List[Dict], output: Output) -> None:
         print(format_string.format(**dict(context, **dct)), file=output)
 
 
-def clip_activities_by_range(
-    start_date: datetime.date,
-    end_date: datetime.date,
-    activities: List[Activity],
-) -> List[Activity]:
-    """Clip a list of Activity with the given range, remove activities
-    which have zero durations
-
-    Parameters
-    ----------
-    start_date : datetime.date
-    end_date : datetime.date
-    activities : list of Activity
-
-    Returns
-    -------
-    clipped: list of Activity
-    """
-    delta = datetime.timedelta()
-    start_dt = datetime.datetime(start_date.year, start_date.month, start_date.day)
-    end_dt = datetime.datetime(end_date.year, end_date.month, end_date.day, 23, 59, 59, 99999)
-    new_activities = []
-    for activity in activities:
-        clipped = activity.clip(start_dt, end_dt)
-        if clipped.duration > delta:
-            new_activities.append(clipped)
-    return new_activities
-
-
 def filter_activities_by_type(activities: List[Activity], activity_type: str) -> List[Activity]:
     """Filter a list of Activity with the given activity type.
 
